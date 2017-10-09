30eedd32b10ab6fa15402e144c335ba309e0b0e0
budmc29 <chirica.mugurel@gmail.com>
2017-10-09 22:59:54 +0100

    Move design patters to patters repo

--- design_patters/interface_use.js
+++
@@ -1,9 +0,0 @@
-var ResultSet = new Interface('ResultSet', ['getDate', 'getResults']);
-
-var ResultFormatter = function(resultsObject) {
-  Interface.ensureImplements(resultsObject, ResultSet);
-  this.resultsObject = resultsObject;
-};
-
-ResultFormatter.prototype.renderResults = function() {
-};
