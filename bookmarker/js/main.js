39d35d80f7408eb88bb941eb5dd443e80ef6d926
budmc29 <chirica.mugurel@gmail.com>
2017-10-21 01:07:21 +0100

    Display saved bookmarks

--- bookmarker/js/main.js
+++ bookmarker/js/main.js
@@ -25,3 +25,21 @@ function saveBookmark(e) {
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 }
+
+function fetchBookmarks() {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  for (var i = 0; i < bookmarks.length; ++i) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML +=
+      '<div class="well">' +
+      '<h3>' + name +
+      '<a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+      '</h3>' +
+      '</div>';
+  }
+}
