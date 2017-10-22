774b470d7980dba42b92e73b51451fb558672bfd
budmc29 <chirica.mugurel@gmail.com>
2017-10-22 01:52:56 +0100

    [Meteor] Use meteor methods instead of insecure

--- meteor/todos/main.js
+++ meteor/todos/main.js
@@ -16,23 +16,18 @@ if (Meteor.isClient) {
     "submit .new-todo": function(event) {
       var text = event.target.text.value;
 
-      Todos.insert({
-        text: text,
-        createdAt: new Date(),
-        userId: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call('addTodo', text);
 
       event.target.text.value = '';
 
       return false;
     },
     "click .toggle-checked": function() {
-      Todos.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call('setChecked', this._id, !this.checked);
     },
     "click .delete-todo": function() {
       if (confirm('Are you sure?')) {
-        Todos.remove(this._id);
+        Meteor.call('deleteTodo', this._id);
       }
     }
   });
@@ -41,3 +36,24 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTodo: function(text) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId) {
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked) {
+    Todos.update(todoId, {$set: {checked: setChecked}});
+  }
+});
