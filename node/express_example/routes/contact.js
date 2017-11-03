533c5ce07872c67ccb12f79d693686b336db1b95
budmc29 <chirica.mugurel@gmail.com>
2017-11-03 00:20:38 +0000

    [Express Example] Send email from contact page

--- node/express_example/routes/contact.js
+++ node/express_example/routes/contact.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'user@example.com',
+      pass: 'something'
+    }
+  });
+
+  var mailOptions = {
+    from: 'User Example <user@example.com>',
+    to: 'recipient@example.com',
+    subject: 'Website Submission',
+    text: 'New submisson: Name: ' + req.body.name + ', Email: ' + req.body.email + ', Message: ' + req.body.message
+  }
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message sent' + info.response);
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
