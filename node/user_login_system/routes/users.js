da3776eb8533b8ff07f1b3f33d08b12f22824259
budmc29 <chirica.mugurel@gmail.com>
2017-11-04 13:24:59 +0000

    [User Login System] Add register page validation

--- node/user_login_system/routes/users.js
+++ node/user_login_system/routes/users.js
@@ -14,4 +14,65 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login'});
 });
 
+router.post('/register', function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password_confirmation = req.body.password_confirmation;
+
+  // Check for image field
+  if (req.files.profileimage) {
+    console.log('uploading file');
+
+    var profileImageOriginal = req.files.profileimage.originalname;
+    var profileImageName = req.files.profileimage.name;
+    var profileImageMime = req.files.profileimage.mimetype;
+    var profileImagePath = req.files.profileimage.path;
+    var profileImageExtension = req.files.profileimage.extension;
+    var profileImageSize = req.files.profileimage.size;
+  } else {
+    var profileImageName = 'default_image.png';
+  }
+
+  // Form validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password_confirmation', 'Passwords do not match').equals(password);
+
+  // Check for errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errors: errors,
+      name: name,
+      email: email,
+      username: username
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileImageName
+    });
+
+    // Create user
+    user.createUser(newUser, function(error, user) {
+      if (err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success', 'You are now registered and may log in.');
+
+    res.location('/');
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
