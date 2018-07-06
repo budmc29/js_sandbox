2dd354e92b1e110f520d6560abab869323ef1b15
budmc29 <chirica.mugurel@gmail.com>
2018-07-06 13:47:49 +0100

    Add sass support

--- react/todo_redo/webpack.config.js
+++ react/todo_redo/webpack.config.js
@@ -1,8 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development', // or 'production'
-  entry: './src/index.jsx',
+  entry: {
+    main: './src/index.jsx',
+    style: './src/index.scss'
+  },
   devtool: 'eval-source-map',
   module: {
     rules: [
@@ -11,11 +15,20 @@ module.exports = {
         exclude: /\/node_modules\//,
         loader: 'babel-loader'
       },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin(/* optional config here */)
   ]
 }
