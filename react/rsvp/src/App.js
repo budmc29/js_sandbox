fe622642b95bc389d5847e55cd3605968fbba5ae
budmc29 <chirica.mugurel@gmail.com>
2018-09-08 22:55:22 +0100

    Extract counter

--- react/rsvp/src/App.js
+++ react/rsvp/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import GuestList from './GuestList';
+import Counter from './Counter';
 
 class App extends Component {
   state = {
@@ -94,7 +96,17 @@ class App extends Component {
 
   getTotalInvited = () => this.state.guests.length;
 
+  getAttendingGuests = () =>
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total,
+      0
+    );
+
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUnconfirmed = totalInvited - numberAttending;
+
     return (
       <div className="App">
         <header>
@@ -119,22 +131,12 @@ class App extends Component {
                 checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+          />
 
           <GuestList
             guests={this.state.guests}
