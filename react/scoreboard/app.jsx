d8308f526c2f3f1dee3d87353b9aeead91b08fa3
budmc29 <chirica.mugurel@gmail.com>
2018-09-09 14:34:58 +0100

    Add Scoreboard

---
+++ react/scoreboard/app.jsx
@@ -0,0 +1,12 @@
+const Application = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>
+        Sit ab assumenda fugit exercitationem quibusdam quo? Alias voluptates eius molestias excepturi excepturi Eos voluptas libero dolore ullam repellendus Libero aliquid recusandae?
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.querySelector('#container'));
