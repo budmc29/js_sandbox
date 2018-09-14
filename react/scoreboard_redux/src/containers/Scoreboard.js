22331cdb5b9874cae71901d78e0e57949c00be56
budmc29 <chirica.mugurel@gmail.com>
2018-09-14 17:28:26 +0100

    Extract Stats component

--- react/scoreboard_redux/src/containers/Scoreboard.js
+++ react/scoreboard_redux/src/containers/Scoreboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stopwatch from '../components/Stopwatch';
+import Stats from '../components/Stats';
 
 const INITIAL_STATE = {
   players: [
@@ -73,34 +74,6 @@ function Header(props) {
 }
 
 Header.propTypes = {
-  players: React.PropTypes.array.isRequired,
-};
-
-// Move to components/Stats.js
-// -----------------------------------------------------------------------
-function Stats(props) {
-  const playerCount = props.players.length;
-  const totalPoints = props.players.reduce(function(total, player) {
-    return total + player.score;
-  }, 0);
-
-  return (
-    <table className="stats">
-      <tbody>
-        <tr>
-          <td>Players:</td>
-          <td>{playerCount}</td>
-        </tr>
-        <tr>
-          <td>Total Points:</td>
-          <td>{totalPoints}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-Stats.propTypes = {
   players: React.PropTypes.array.isRequired,
 };
 
