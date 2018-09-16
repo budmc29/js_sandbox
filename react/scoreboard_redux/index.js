d7a3bcc95e127bcf438fbf75e8378cf5ec400582
budmc29 <chirica.mugurel@gmail.com>
2018-09-16 20:26:32 +0100

    Create the redux store

--- react/scoreboard_redux/index.js
+++ react/scoreboard_redux/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
 import Scoreboard from './src/containers/Scoreboard';
 
-render(<Scoreboard />, document.querySelector('#root'));
+const store = createStore(
+  PlayerReducer
+);
+
+render(
+  <Provider store={store}>
+    <Scoreboard />
+  </Provider>,
+  document.querySelector('#root')
+);
