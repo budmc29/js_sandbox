adc97a8b9710722b7b7dc680cc84775c26efc750
budmc29 <chirica.mugurel@gmail.com>
2019-04-26 16:23:29 +0100

    Extract utils file

--- react/stars-game/src/App.js
+++ react/stars-game/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import utils from './utils.js';
 
 const App = () => {
+  const [stars, setStars] = React.useState(utils.random(1, 9));
+
   return (
     <div className="game">
       <div className="help">
@@ -9,26 +12,14 @@ const App = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          {utils.range(1, stars).map(starId =>
+            <div key={starId} className="star" />
+          )}
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {utils.range(1, 9).map(number =>
+            <button key={number} className="number">{number}</button>
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
