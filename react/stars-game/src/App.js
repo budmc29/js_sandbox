711cd0e01e95f8d56234a8cadb21289428b2ee88
budmc29 <chirica.mugurel@gmail.com>
2019-04-26 16:09:18 +0100

    Add ui code

--- react/stars-game/src/App.js
+++ react/stars-game/src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return ('Hello World');
-}
+const App = () => {
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+        </div>
+        <div className="right">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
 
 export default App;
--- react/stars-game/src/index.js
+++ react/stars-game/src/index.js
@@ -3,4 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(max * Math.random()),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length)];
+  },
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
